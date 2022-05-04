ec601fae974a6a8f66377961687d1be974bd0348
Harsh Makwana <harshmakwana181@gmail.com>
2022-05-04 11:37:46 -0400

    Add files via upload

---
+++ Code/cis552project/SQLDataType.java
@@ -0,0 +1,6 @@
+package cis552project;
+
+
+public enum SQLDataType {
+    STRING,VARCHAR,CHAR,INT,DECIMAL, DATE
+}
---
+++ Code/cis552project/iterator/LimitIT.java
@@ -0,0 +1,58 @@
+
+package cis552project.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import net.sf.jsqlparser.statement.select.Limit;
+
+
+public class LimitIT extends BaseIT {
+	Limit limit;
+	long current_position = 0;
+	List<Tuple> resultTuples = new ArrayList<>();
+	BaseIT result = null;
+	Iterator<Tuple> resIT;
+	TableResult tableResult;
+
+	LimitIT(Limit limit, BaseIT result) {
+		this.limit = limit;
+		this.result = result;
+	}
+
+	@Override
+	public TableResult getNext() {
+		return tableResult;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if (result == null) {
+			return false;
+		}
+
+		long rowsRequired = limit.getRowCount() - current_position;
+		while (rowsRequired > 0 && result.hasNext()) {
+			tableResult = result.getNext();
+			if (rowsRequired > tableResult.resultTuples.size()) {
+				current_position += tableResult.resultTuples.size();
+				return true;
+			} else {
+				List<Tuple> tupleList = new ArrayList<>();
+				for (long i = rowsRequired; i > 0; i = limit.getRowCount() - current_position) {
+					current_position++;
+					tupleList.add(tableResult.resultTuples.get((int) i));
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void reset() {
+		current_position = 0;
+		result.reset();
+	}
+
+}
